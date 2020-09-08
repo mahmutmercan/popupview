369ef4e6d8c673b3f2a6137d99afe055f63b5d61
mahmutMercan <mahmutmercan12@gmail.com>
2020-09-08 13:39:55 +0300

    ss
    ss

---
+++ popupview/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  popupview
+//
+//  Created by İlker isa Mercan on 8.09.2020.
+//  Copyright © 2020 Mahmut Mercan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+            let pop = Popup()
+            self.view.addSubview(pop)
+            
+        }
+
+    }
+
+
+}
+
---
+++ popupview/popup.swift
@@ -0,0 +1,95 @@
+//
+//  popup.swift
+//  popupview
+//
+//  Created by İlker isa Mercan on 8.09.2020.
+//  Copyright © 2020 Mahmut Mercan. All rights reserved.
+//
+
+import UIKit
+class Popup: UIView {
+    fileprivate let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 28, weight: UIFont.Weight.bold)
+        label.text = "1 credit"
+        label.textAlignment = .center
+        return label
+    }()
+
+    fileprivate let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.bold)
+        label.text = "in app purchase for one book credit"
+        label.textAlignment = .center
+        label.numberOfLines = 3
+        return label
+    }()
+    fileprivate let container: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = .white
+        v.layer.cornerRadius = 24
+        return v
+        
+    }()
+
+    fileprivate lazy var stack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        return stack
+    }()
+    
+    @objc fileprivate func animateOut() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.container.transform = CGAffineTransform(translationX: 0, y: -self.frame.height)
+            self.alpha = 0
+        }) { (complete) in
+            if complete {
+                self.removeFromSuperview()
+            }
+        }
+    }
+    
+    @objc fileprivate func animateIn() {
+        self.container.transform = CGAffineTransform(translationX: 0, y: -self.frame.height)
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            self.container.transform = .identity
+            self.alpha = 1
+        })
+    }
+
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(animateOut)))
+        self.backgroundColor = UIColor.gray.withAlphaComponent(0.6)
+        self.frame = UIScreen.main.bounds
+        self.addSubview(container)
+        
+        container.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        container.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        container.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.7).isActive = true
+        container.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.45).isActive = true
+        
+        container.addSubview(stack)
+        stack.leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
+        stack.trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
+        stack.centerYAnchor.constraint(equalTo: container.centerYAnchor).isActive = true
+        stack.heightAnchor.constraint(equalTo: container.heightAnchor, multiplier: 0.5).isActive = true
+        
+        animateIn()
+
+
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
